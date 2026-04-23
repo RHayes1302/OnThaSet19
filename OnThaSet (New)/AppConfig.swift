91b9a326206b8bd55597a37077f827f01679b224
RHayes1302 <ramone.hayes@yahoo.com>
2026-04-23 15:03:35 -0700

    Submittal #4

---
+++ OnThaSet (New)/AppConfig.swift
@@ -0,0 +1,8 @@
+//
+//  AppConfig.swift
+//  OnThaSet (New)
+//
+//  Created by Ramone Hayes on 4/23/26.
+//
+
+import Foundation
---
+++ OnThaSet (New)/LoginView/EmailAuthView.swift
@@ -0,0 +1,8 @@
+//
+//  EmailAuthView.swift
+//  OnThaSet (New)
+//
+//  Created by Ramone Hayes on 4/22/26.
+//
+
+import Foundation
